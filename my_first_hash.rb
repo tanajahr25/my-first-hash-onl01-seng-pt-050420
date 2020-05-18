948d308e2427184f9efa9db7adfa9e2d80e77ad2
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-05-18 03:45:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,8 +4,8 @@ my_hash = {"name" => "Tanajah", "weight" => "under 150 lbs"}
   
 end
 
-def shipping_manifes
-  shipping_manifest = {"whale bone corsets" => "5", "porcelain vases" => 2, "oil paintings"=> 3}
+def shipping_manifest
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings"=> 3}
   
   
 end
