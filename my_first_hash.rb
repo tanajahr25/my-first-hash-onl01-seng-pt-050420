367f224673bbfa701f87bf85950054ccbea4e627
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-05-18 03:51:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,6 +7,6 @@ end
 def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   
-  shipping_manifest
+  shipping_manifest[oil paintings]
 end
 
